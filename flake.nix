14bac95425f8e0a8cd4b554e85ab4ff252beb385
ottorohenkohl <hey@ottorohenkohl.de>
2025-01-10 09:19:26 +0100

    feat: adding whatsapp

--- flake.nix
+++ flake.nix
@@ -33,6 +33,7 @@
                             "microsoft-outlook"
                             "pycharm"
                             "webstorm"
+                            "whatsapp"
                         ];
 
                         enable = true;
